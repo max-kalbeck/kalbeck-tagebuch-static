05296a3699f36d3dd56716797d472a8e32f7b225
fsanzl <>
2026-07-09 16:01:56 +0200

    It finds entities in data files and purges relations.xml from unrelated relations

--- pyscripts/purge_relations.py
+++ pyscripts/purge_relations.py
@@ -3,39 +3,73 @@ import argparse
 import re
 from lxml import etree
 
-def extract_pmb_refs(xmlfile):
-    tree = etree.parse(xmlfile)
-
+def extract_pmb_refs(xmlfiles):
     pattern = re.compile(r"#pmb([^\s]+)")
-
     refs = set()
 
-    for elem in tree.xpath("//*[@ref]"):
-        ref = elem.get("ref")
-        refs.update(pattern.findall(ref))
+    for xmlfile in xmlfiles:
+        tree = etree.parse(xmlfile)
 
-    return refs
+        for elem in tree.xpath("//*[@ref]"):
+            ref = elem.get("ref")
+            refs.update(pattern.findall(ref))
+    return {f"#{x}" for x in refs}
+
+def filter_relations(relations_file, output_file, pmb_refs):
+    tree = etree.parse(relations_file)
+
+    ns = {"tei": "http://www.tei-c.org/ns/1.0"}
+
+    relations = tree.xpath("//tei:relation", namespaces=ns)
+
+    for rel in relations:
+        active = rel.get("active", "").split()
+        passive = rel.get("passive", "").split()
+
+        
+        if not (pmb_refs.intersection(active) or pmb_refs.intersection(passive)):
+            rel.getparent().remove(rel)
+    etree.indent(tree, space="  ")
+    tree.write(
+        output_file,
+        encoding="UTF-8",
+        xml_declaration=True,
+        pretty_print=True
+    )
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Extract all @ref values matching #pmbX from TEI XML files"
+        description="Extract PMB refs from TEI files and filter relations.xml"
     )
+
     parser.add_argument(
         "xmlfiles",
         nargs="+",
-        help="One or more TEI XML files"
+        help="TEI XML files containing @ref attributes"
     )
+
+    parser.add_argument(
+        "-r", "--relations",
+        required=True,
+        help="relations.xml file"
+    )
+
+    parser.add_argument(
+        "-o", "--output",
+        required=True,
+        help="output filtered relations XML file"
+    )
+
     args = parser.parse_args()
+    
+    pmb_refs = extract_pmb_refs(args.xmlfiles)
 
-    pmb_refs = set()
-
-    for xmlfile in args.xmlfiles:
-        pmb_refs.update(extract_pmb_refs(xmlfile))
-        
-    for ref in sorted(pmb_refs):
-        print(ref)
-
+    filter_relations(
+        args.relations,
+        args.output,
+        pmb_refs
+    )
 
 
 if __name__ == "__main__":
